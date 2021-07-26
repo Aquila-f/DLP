4c1503a44c7c161e814ee5f8c1e5c1bf9125368f
qwert1234 <qwertgb1234@gmail.com>
2021-07-27 02:43:44 +0800

    f

--- lab2/01.py
+++ lab2/01.py
@@ -125,28 +125,28 @@ class DeepConvNet(nn.Module):
             nn.Conv2d(25, 25, kernel_size=(2,1)),
             nn.BatchNorm2d(25, eps=1e-5, momentum=0.1),
             activation_funcchoose(self.act_funct),
-            nn.MaxPool2d(kernel_size=(1,2),stride=(1,2)),
+            nn.MaxPool2d(kernel_size=(1,2)),
             nn.Dropout(0.5)
         )
         self.conv3 = nn.Sequential(
             nn.Conv2d(25, 50, kernel_size=(1,5)),
             nn.BatchNorm2d(50, eps=1e-5, momentum=0.1),
             activation_funcchoose(self.act_funct),
-            nn.MaxPool2d(kernel_size=(1,4),stride=(1,4)),
+            nn.MaxPool2d(kernel_size=(1,2)),
             nn.Dropout(0.5)
         )
         self.conv4 = nn.Sequential(
             nn.Conv2d(50, 100, kernel_size=(1,5)),
             nn.BatchNorm2d(100, eps=1e-5, momentum=0.1),
             activation_funcchoose(self.act_funct),
-            nn.MaxPool2d(kernel_size=(1,8),stride=(1,8)),
+            nn.MaxPool2d(kernel_size=(1,2)),
             nn.Dropout(0.5)
         )
         self.conv5 = nn.Sequential(
             nn.Conv2d(100, 200, kernel_size=(1,5)),
             nn.BatchNorm2d(200, eps=1e-5, momentum=0.1),
             activation_funcchoose(self.act_funct),
-            nn.MaxPool2d(kernel_size=(1,10),stride=(1,10)),
+            nn.MaxPool2d(kernel_size=(1,2)),
             nn.Dropout(0.5),
             nn.Flatten()
         )
