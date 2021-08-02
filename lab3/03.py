cca0f738f9cb44bfebf9c9d3226e04fd4664d627
qwert1234 <qwertgb1234@gmail.com>
2021-08-02 11:28:32 +0800

    d

--- lab3/03.py
+++ lab3/03.py
@@ -38,7 +38,7 @@ class RetinopathyLoader(data.Dataset):
         img = Image.open(path)
         
         preprocess = transforms.Compose([
-        #     transforms.Resize(512),
+            transforms.Resize(128),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
